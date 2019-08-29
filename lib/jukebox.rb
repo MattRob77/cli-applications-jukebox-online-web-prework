c8cb62284d9766817394bf5ab2a9533f986509e8
Matthew Robinson <matt_robinson1@yahoo.com>
2019-08-29 16:17:35 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,6 +26,7 @@ def play(songs)
    if songs.include?(input)
      puts "Playing #{input}"
      elsif (0..songs.length).include?(input.to_i - 1)
-     
+      puts "Playing #{songs[input.to_i - 1]}"
+      
   
 end 
